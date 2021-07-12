75afb7389a26b03c4251896a912ad5fe6232e208
KuhakuPixel <Nicholaspixels@gmail.com>
2021-07-12 16:48:07 +0700

    update Game manager

--- GameJamHell/Assets/GameManager.cs
+++ GameJamHell/Assets/GameManager.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Events;
 public class GameManager : MonoBehaviour
 {
+    UnityEvent onGameOver;
+    UnityEvent onGameStart;
+    PlayerScript playerScript;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerScript = FindObjectOfType<PlayerScript>();
+        onGameStart.Invoke();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //listening 
+        if (playerScript.GetUnsealedPuppetCount() == 0)
+        {
+            onGameOver.Invoke();
+        }
     }
     public void OnGeneratorComplete()
     {
